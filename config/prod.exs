69416480a90e7ab5178fb3eda8c8eae621fc2442
Jude Nicholl <jnicholl978@gmail.com>
2017-07-11 17:44:00 +0200

    another commit with zi

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :hello, Hello.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "guarded-shore-81747.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
@@ -23,7 +23,7 @@ config :logger, level: :info
 
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
